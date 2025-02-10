574b64b505e8055b95d011c0d2d3183875cf4819
Em Pongpat <empongpat@gmail.com>
2025-02-10 23:48:12 +0800

    feat: prototype a forecast view

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            VStack {
+                Text("Mon")
+                Image(systemName: "sun.max.fill")
+                    .foregroundStyle(Color.yellow)
+                Text("High: 70")
+                Text("Low: 50")
+            }
+            .padding()
+            
+            VStack {
+                Text("Tue")
+                Image(systemName: "cloud.rain.fill")
+                    .foregroundStyle(Color.blue)
+                Text("High: 60")
+                Text("Low: 40")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
